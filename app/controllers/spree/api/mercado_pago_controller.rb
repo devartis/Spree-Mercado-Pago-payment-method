7c1a82acf399adf284203794fd178d8db3c72583
Leandro Gomez <lgomez@devartis.com>
2014-09-26 16:26:37 -0300

    add api for create payment

---
+++ app/controllers/spree/api/mercado_pago_controller.rb
@@ -0,0 +1,91 @@
+module Spree
+  module Api
+    class MercadoPagoController < BaseController
+
+      before_filter :find_order
+      before_filter :verify_payment_state, only: :payment
+
+      # If the order is in 'payment' state, redirects to Mercado Pago Checkout page
+      def payment
+        mp_payment = @order.current_payment
+        mp_payment.source = MercadoPagoSource.create!
+        mp_payment.save!
+
+        if create_preferences(mp_payment)
+          render json: {redirect_url: provider.redirect_url, ok: true}
+        else
+          render json: {ok: false}
+        end
+      end
+
+      private
+
+      def find_order
+        @order = Spree::Order.find_by!(number: params[:id])
+      end
+
+      def create_preferences(mp_payment)
+        preferences = create_preference_options(@order, mp_payment, get_back_urls(mp_payment))
+
+        Rails.logger.info "Sending preferences to MercadoPago"
+        Rails.logger.info "#{preferences}"
+
+        provider.create_preferences(preferences)
+      end
+
+      def create_preference_options(order, payment, callbacks)
+        builder = MercadoPago::OrderPreferencesBuilder.new order, payment, callbacks, payer_data
+
+        return builder.preferences_hash
+      end
+
+      def payment_method
+        @payment_method ||= ::PaymentMethod::MercadoPago.find (params[:payment_method_id])
+      end
+
+      def provider
+        @provider ||= payment_method.provider({:payer => payer_data})
+      end
+
+      # Get payer info for sending within Mercado Pago request
+      def payer_data
+        email = get_email
+        {email: email}
+      end
+
+      # Get email for using in Mercado Pago request
+      def get_email
+        @order.email
+      end
+
+      # Get urls callbacks.
+      # If the current 'payment method' haven't any callback, the default will be used
+      def get_back_urls(mp_payment)
+        success_url = payment_method.preferred_success_url
+        pending_url = payment_method.preferred_pending_url
+        failure_url = payment_method.preferred_failure_url
+
+        get_params = {
+            order_number: @order.number,
+            payment_identifier: mp_payment.identifier
+        }
+
+        success_url = spree.mercado_pago_success_url(get_params) if success_url.empty?
+        pending_url = spree.mercado_pago_pending_url(get_params) if pending_url.empty?
+        failure_url = spree.mercado_pago_failure_url(get_params) if failure_url.empty?
+
+        {
+            success: success_url,
+            pending: pending_url,
+            failure: failure_url,
+        }
+      end
+
+      def verify_payment_state
+        redirect_to root_path unless @order.payment_confirmation?
+      end
+
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 # -*- encoding : utf-8 -*-
 Spree::Core::Engine.routes.draw do
+
+  namespace :api, :defaults => {:format => 'json'} do
+    resources :orders do
+      member do
+        post 'mercado_pago/payment', to: 'mercado_pago#payment', as: :mercado_pago_payment_api
+      end
+
+    end
+  end
+
   scope "/mercado_pago", controller: :mercado_pago do
     get :success, as: :mercado_pago_success 
     get :pending, as: :mercado_pago_pending 
