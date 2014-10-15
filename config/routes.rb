75204beb008e04cca0d28b44aaac80b4e579b526
Leandro Gomez <leandro.gz73@gmail.com>
2014-10-15 12:59:26 -0300

    Remove all routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,8 @@ Spree::Core::Engine.routes.draw do
       end
 
     end
+    post :notification, to: 'mercado_pago#payment', as: :mercado_pago_notification
+    get :notification, to: 'mercado_pago#payment', as: :mercado_pago_notification_get
   end
 
-  scope "/mercado_pago", controller: :mercado_pago do
-    post :notification, as: :mercado_pago_notification
-    get :notification, as: :mercado_pago_notification_get
-  end
 end
