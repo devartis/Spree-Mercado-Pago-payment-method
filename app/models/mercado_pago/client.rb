65f5cfd3bd1bfe6425d33ab543bf8958046ffc40
Matias Garcia <mgarcia@devartis.com>
2015-02-12 09:31:14 -0300

    Adjust send_money_request to new post method

--- app/models/mercado_pago/client.rb
+++ app/models/mercado_pago/client.rb
@@ -6,77 +6,73 @@ require 'mercado_pago/client/api'
 
 module MercadoPago
 
-class Client
-  # These three includes are because of the user of line_item_description from
-  # ProductsHelper
+  class Client
+    # These three includes are because of the user of line_item_description from ProductsHelper
 
-  include Authentication
-  include Preferences
-  include API
+    include Authentication
+    include Preferences
+    include API
 
+    attr_reader :errors
+    attr_reader :auth_response
+    attr_reader :preferences_response
 
-  attr_reader :errors
-  attr_reader :auth_response
-  attr_reader :preferences_response
+    def initialize(payment_method, options={})
+      @payment_method = payment_method
+      @api_options = options.clone
+      @errors = []
+    end
 
-  def initialize(payment_method, options={})
-    @payment_method = payment_method
-    @api_options = options.clone
-    @errors = []
-  end
+    def get_external_reference(mercado_pago_id)
+      response = send_notification_request mercado_pago_id
+      if response
+        response['collection']['external_reference']
+      end
+    end
 
+    def get_payment_status(external_reference)
+      response = send_search_request({:external_reference => external_reference, :access_token => access_token})
 
-  def get_external_reference(mercado_pago_id)
-    response = send_notification_request mercado_pago_id
-    if response
-      response['collection']['external_reference']
+      if response['results'].empty?
+        "pending"
+      else
+        response['results'][0]['collection']['status']
+      end
+    end
+
+    private
+
+    def log_error(msg, response, request, result)
+      Rails.logger.info msg
+      Rails.logger.info "response: #{response}."
+      Rails.logger.info "request args: #{request.args}."
+      Rails.logger.info "result #{result}."
+    end
+
+    def send_notification_request(mercado_pago_id)
+      url = create_url(notifications_url(mercado_pago_id), access_token: access_token)
+      options = {:content_type => 'application/x-www-form-urlencoded', :accept => 'application/json'}
+      get(url, options, quiet: true)
+    end
+
+    def send_search_request(params, options={})
+      url = create_url(search_url, params)
+      options = {:content_type => 'application/x-www-form-urlencoded', :accept => 'application/json'}
+      get(url, options)
+    end
+
+    def send_money_request(payer_email, amount, description)
+      url = create_url(money_request_url, access_token: access_token)
+      headers = {:content_type => 'application/json', :accept => 'application/json'}
+      params = {
+          currency_id: 'ARS',
+          payer_email: payer_email,
+          amount: amount,
+          description: description,
+          concept_type: 'off_platform'
+      }
+      post url, params, headers
+      ActiveSupport::JSON.decode(response)
     end
   end
-
-  def get_payment_status(external_reference)
-    response = send_search_request({:external_reference => external_reference, :access_token => access_token})
-
-    if response['results'].empty?
-      "pending"
-    else
-      response['results'][0]['collection']['status']
-    end
-  end
-
-  private
-
-
-  def log_error(msg, response, request, result)
-    Rails.logger.info msg
-    Rails.logger.info "response: #{response}."
-    Rails.logger.info "request args: #{request.args}."
-    Rails.logger.info "result #{result}."
-  end
-
-  def send_notification_request(mercado_pago_id)
-    url = create_url(notifications_url(mercado_pago_id), access_token: access_token)
-    options = {:content_type => 'application/x-www-form-urlencoded', :accept => 'application/json'}
-    get(url, options, quiet: true)
-  end
-
-  def send_search_request(params, options={})
-    url = create_url(search_url, params)
-    options = {:content_type => 'application/x-www-form-urlencoded', :accept => 'application/json'}
-    get(url, options)
-  end
-
-  def send_money_request(payer_email, amount, description)
-    url = create_url(money_request_url, access_token: access_token)
-    headers = {:content_type => 'application/json', :accept => 'application/json'}
-    params = {
-        currency_id: 'ARS',
-        payer_email: payer_email,
-        amount: amount,
-        description: description,
-        concept_type: 'off_platform'}.to_json
-    post url, params, headers
-    ActiveSupport::JSON.decode(response)
-  end
-end
-
 end
