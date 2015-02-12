de2891a03d9ea27668c6e6f987289bdc47fc4223
Matias Garcia <mgarcia@devartis.com>
2015-02-12 11:02:57 -0300

    Add methods

--- app/models/payment_method/mercado_pago_money_request.rb
+++ app/models/payment_method/mercado_pago_money_request.rb
@@ -4,4 +4,41 @@ class PaymentMethod::MercadoPagoMoneyRequest < Spree::PaymentMethod
   preference :client_secret, :string
 
   include Provider
+
+  def payment_profiles_supported?
+    false
+  end
+
+  def can_void?(payment)
+    !payment.void?
+  end
+
+  def can_capture?(payment)
+    payment.pending? || payment.checkout?
+  end
+
+  def auto_capture?
+    false
+  end
+
+  def payment_source_class
+    nil
+  end
+
+  def authorize(amount, response_code, gateway_options)
+    ActiveMerchant::Billing::Response.new(true, 'MercadoPagoMoneyRequest payment authorized', {})
+  end
+
+  private
+  def pending?(status)
+    status == 'pending'
+  end
+
+  def failed?(status)
+    status == 'rejected' or status == 'cancelled'
+  end
+
+  def accepted?(status)
+    status == 'accepted'
+  end
 end
