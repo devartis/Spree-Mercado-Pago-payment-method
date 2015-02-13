8e7493c4a9d1af3511cceeac5523aada8ce8c8a4
Matias Garcia <mati92.ss@gmail.com>
2015-02-13 11:02:21 -0300

    Change migration

--- db/migrate/20150212143541_create_mercado_pago_manual_sources.rb
+++ db/migrate/20150212143541_create_mercado_pago_manual_sources.rb
@@ -2,11 +2,8 @@ class CreateMercadoPagoManualSources < ActiveRecord::Migration
   def change
     create_table :mercado_pago_manual_sources do |t|
       t.integer :mercado_pago_id
-      t.decimal :amount, precision: 10, scale: 2, default: 0.0
       t.string  :payer_email
-      t.integer :payer_id
       t.string  :external_reference
-      t.string  :pref_id
       t.integer :user_id
       t.integer :payment_method_id
 
