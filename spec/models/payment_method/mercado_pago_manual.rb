2d8c3050209dd1d373ec87f0eb280c8107164d2d
Matias Garcia <mgarcia@devartis.com>
2015-02-18 16:06:12 -0300

    refactor spec

--- spec/models/payment_method/mercado_pago_manual.rb
+++ spec/models/payment_method/mercado_pago_manual.rb
@@ -1,30 +1,29 @@
 require 'spec_helper'
 
+shared_examples 'should not be captured' do
+  it 'should not be captured' do
+    payment.should_not_receive(:capture!)
+    subject.try_capture(payment)
+  end
+end
+
 describe PaymentMethod::MercadoPagoManual do
   let (:payment) { create :mp_manual_payment }
   subject { payment.payment_method }
 
   before(:each) { mock_authenticate subject.preferred_client_id, subject.preferred_client_secret }
 
-  context 'a pending money request' do
+  context 'with a pending money request' do
     before(:each) { mock_get_money_request payment.source.mercado_pago_id, 'pending' }
-
-    it 'should not be captured' do
-      payment.should_not_receive(:capture!)
-      subject.try_capture(payment)
-    end
+    include_examples 'should not be captured'
   end
 
-  context 'an accepted money request' do
+  context 'with an accepted money request' do
     before(:each) { mock_get_money_request payment.source.mercado_pago_id, 'accepted' }
 
     context 'with a pending mercado pago payment' do
       before(:each) { mock_get_payment_status }
-
-      it 'should not be captured' do
-        payment.should_not_receive(:capture!)
-        subject.try_capture(payment)
-      end
+      include_examples 'should not be captured'
     end
   end
 end
