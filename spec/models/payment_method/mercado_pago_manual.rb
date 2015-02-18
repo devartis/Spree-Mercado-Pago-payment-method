cb36672853b91c3f5825449bb51befe492111682
Matias Garcia <mgarcia@devartis.com>
2015-02-18 15:46:36 -0300

    Refactor

--- spec/models/payment_method/mercado_pago_manual.rb
+++ spec/models/payment_method/mercado_pago_manual.rb
@@ -6,6 +6,7 @@ describe PaymentMethod::MercadoPagoManual do
 
   before :each do
     mock_authenticate(subject.preferred_client_id, subject.preferred_client_secret)
+    mock_get_money_request(payment.source.mercado_pago_id, 'pending')
   end
 
   context 'a pending money request' do
--- spec/support/mercado_pago_api_support.rb
+++ spec/support/mercado_pago_api_support.rb
@@ -1,17 +1,19 @@
 module MercadoPagoApiSupport
   def mock_authenticate(client_id, client_secret)
-    stub_request(:post, 'https://api.mercadolibre.com/oauth/token').
-        with(:body => {'client_id' => client_id, 'client_secret' => client_secret, 'grant_type' => "client_credentials"},
-             :headers => {'Accept' => 'application/json', 'Accept-Encoding' => 'gzip, deflate', 'Content-Length' => '63', 'Content-Type' => 'application/x-www-form-urlencoded', 'User-Agent' => 'Ruby'}).
-        to_return(:status => 200, :body => authenticated_json, :headers => {})
+    headers = default_api_headers.merge({ 'Content-Length' => '63', 'Content-Type' => 'application/x-www-form-urlencoded' })
+    mock_api_call mercado_pago_api_url, '/oauth/token',
+                  headers: headers,
+                  method: :post,
+                  body: {'client_id' => client_id, 'client_secret' => client_secret, 'grant_type' => "client_credentials"},
+                  return_body: authenticated_json
   end
 
   def mock_get_money_request(money_request_id, response = nil)
     response ||= money_request_json
-    access_token = authenticated_json['access_token']
-    stub_request(:get, "https://api.mercadolibre.com/money_requests/#{money_request_id}?access_token=#{access_token}").
-        with(:headers => {'Accept' => 'application/json', 'Accept-Encoding' => 'gzip, deflate', 'User-Agent' => 'Ruby'}).
-        to_return(:status => 200, :body => money_request_json, :headers => {})
+    access_token = JSON.parse(authenticated_json, symbolize_names: true)[:access_token]
+    mock_api_call mercado_pago_api_url, "/money_requests/#{money_request_id}?access_token=#{access_token}",
+                  method: :get,
+                  return_body: money_request_json
   end
 
   def money_request_json(status = nil)
@@ -22,11 +24,41 @@ module MercadoPagoApiSupport
 
   private
 
+  def mock_api_call(host, path, options={})
+    headers = options[:headers] || default_api_headers
+    return_headers = options[:return_headers] || default_return_headers
+    status = options[:status] || default_status_code
+    return_body = options[:return_body] || ''
+    method = options[:method] || :get
+    body = options[:body] || ''
+
+    stub_request(method, "#{host}#{path}").
+        with(headers: headers, body: body).
+        to_return(status: status, body: return_body, headers: return_headers)
+  end
+
   def open_fixture(file_name)
-    JSON.parse(File.open("#{::Rails.root.to_s}/../fixtures/#{file_name}", 'r').read, symbolize_names: true)
+    File.open("#{::Rails.root.to_s}/../fixtures/#{file_name}", 'r').read
   end
 
   def authenticated_json
     open_fixture('authenticated.json')
   end
+
+  def mercado_pago_api_url
+    'https://api.mercadolibre.com'
+  end
+
+  def default_api_headers
+    {'Accept' => 'application/json', 'Accept-Encoding' => 'gzip, deflate', 'User-Agent' => 'Ruby'}
+  end
+
+  def default_status_code
+    200
+  end
+
+  def default_return_headers
+    {}
+  end
+
 end
