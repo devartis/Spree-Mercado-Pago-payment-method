221a145db4ace6934667b7fb3acdc32dce3a191f
Matias Garcia <mgarcia@devartis.com>
2015-02-18 13:12:39 -0300

    MercadoPagoApiSupport

---
+++ spec/support/mercado_pago_api_support.rb
@@ -0,0 +1,14 @@
+module MercadoPagoApiSupport
+  def mock_authenticate(client_id, client_secret)
+    stub_request(:post, 'https://api.mercadolibre.com/oauth/token').
+        with(:body => {"client_id" => client_id, "client_secret" => client_secret, "grant_type" => "client_credentials"},
+             :headers => {'Accept' => 'application/json', 'Accept-Encoding' => 'gzip, deflate', 'Content-Length' => '63', 'Content-Type' => 'application/x-www-form-urlencoded', 'User-Agent' => 'Ruby'}).
+        to_return(:status => 200, :body => authenticated_json, :headers => {})
+  end
+
+  private
+
+  def authenticated_json
+    File.open("#{SPEC_ROOT}/../fixtures/authenticated.json", 'r').read
+  end
+end
