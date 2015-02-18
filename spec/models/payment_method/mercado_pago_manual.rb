568f5ad236698c0867b3a34958d15ea2baae578c
Matias Garcia <mgarcia@devartis.com>
2015-02-18 16:00:02 -0300

    more specs

--- spec/models/payment_method/mercado_pago_manual.rb
+++ spec/models/payment_method/mercado_pago_manual.rb
@@ -4,16 +4,27 @@ describe PaymentMethod::MercadoPagoManual do
   let (:payment) { create :mp_manual_payment }
   subject { payment.payment_method }
 
-  before :each do
-    mock_authenticate(subject.preferred_client_id, subject.preferred_client_secret)
-    mock_get_money_request(payment.source.mercado_pago_id, 'pending')
-  end
+  before(:each) { mock_authenticate subject.preferred_client_id, subject.preferred_client_secret }
 
   context 'a pending money request' do
-    it do
+    before(:each) { mock_get_money_request payment.source.mercado_pago_id, 'pending' }
+
+    it 'should not be captured' do
       payment.should_not_receive(:capture!)
       subject.try_capture(payment)
     end
   end
 
+  context 'an accepted money request' do
+    before(:each) { mock_get_money_request payment.source.mercado_pago_id, 'accepted' }
+
+    context 'with a pending mercado pago payment' do
+      before(:each) { mock_get_payment_status }
+
+      it 'should not be captured' do
+        payment.should_not_receive(:capture!)
+        subject.try_capture(payment)
+      end
+    end
+  end
 end
