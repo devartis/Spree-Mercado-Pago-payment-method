824c3b7e7e15f6ada632c2b509728c17c0f3261c
Matias Garcia <mgarcia@devartis.com>
2015-02-19 09:43:27 -0300

    Fix wrong call

--- spec/models/payment_method/mercado_pago_manual.rb
+++ spec/models/payment_method/mercado_pago_manual.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative '../../spec_helper.rb'
 
 shared_examples 'should not be captured' do
   it 'should not be captured' do
@@ -22,12 +22,12 @@ describe PaymentMethod::MercadoPagoManual do
     before(:each) { mock_get_money_request payment.source.mercado_pago_id, 'accepted' }
 
     context 'with a pending mercado pago payment' do
-      before(:each) { mock_get_payment_status 'pending', payment.source.external_reference }
+      before(:each) { mock_get_payment_status payment.source.external_reference, 'pending' }
       include_examples 'should not be captured'
     end
 
     context 'with an approved mercado pago payment' do
-      before(:each) { mock_get_payment_status 'approved', payment.source.external_reference }
+      before(:each) { mock_get_payment_status payment.source.external_reference, 'approved' }
       it 'should be captured' do
         payment.should_receive(:capture!)
         subject.try_capture(payment)
