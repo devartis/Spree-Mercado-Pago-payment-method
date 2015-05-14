d0fafe1e33f84f6979905c55cf1d73bd6f2c65ab
Matias Garcia <mgarcia@devartis.com>
2015-05-14 11:39:53 -0300

    fix namespacing

--- app/models/spree/mercado_pago_manual_source.rb
+++ app/models/spree/mercado_pago_manual_source.rb
@@ -2,5 +2,9 @@ module Spree
   class MercadoPagoManualSource < ActiveRecord::Base
     belongs_to :payment_method
     has_many :payments, as: :source, class_name: '::Spree::Payment'
+
+    def self.table_name
+      'mercado_pago_manual_sources'
+    end
   end
 end
