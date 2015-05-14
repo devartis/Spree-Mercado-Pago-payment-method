3b8e7cd33f500b792fd45cbff3043f6873ade142
Matias Garcia <mgarcia@devartis.com>
2015-05-14 14:37:19 -0300

    allow to receive payer_id or payer_email

--- app/models/spree/mercado_pago/custom_client/payment.rb
+++ app/models/spree/mercado_pago/custom_client/payment.rb
@@ -1,14 +1,25 @@
 class Spree::MercadoPago::CustomClient::Payment < Spree::MercadoPago::CustomClient::Resource
-  def build_create_params(amount, card_token, description, installments, payment_method_id, payer_email)
-    {
+  def build_create_params(amount, card_token, description, installments, options={})
+    if options.keys.size == 2 and options[:payment_method_id] and options[:payer_email]
+      diff_params = {
+          payment_method_id: options[:payment_method_id],
+          payer: [
+              {email: options[:payer_email]}
+          ]
+      }
+    elsif options.keys.size == 1 and options[:payer_id]
+      diff_params = {
+          payer: [
+              {id: options[:payer_id]}
+          ]
+      }
+    end
+    common_params = {
         transaction_amount: amount,
         token: card_token,
         description: description,
         installments: installments,
-        payment_method_id: payment_method_id,
-        payer: [{
-                    email: payer_email
-                }]
     }
+    common_params.merge(diff_params)
   end
 end
