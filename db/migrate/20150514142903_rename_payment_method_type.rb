bf68cf9a5df17017e4610e663afaf2fea37983a2
Matias Garcia <mgarcia@devartis.com>
2015-05-14 12:32:45 -0300

    rename source

--- db/migrate/20150514142903_rename_payment_method_type.rb
+++ db/migrate/20150514142903_rename_payment_method_type.rb
@@ -3,5 +3,7 @@ class RenamePaymentMethodType < ActiveRecord::Migration
     Spree::PaymentMethod.where(type: 'PaymentMethod::MercadoPago').update_all(type: 'Spree::PaymentMethod::MercadoPagoBasic')
     Spree::PaymentMethod.where(type: 'PaymentMethod::MercadoPagoManual').update_all(type: 'Spree::PaymentMethod::MercadoPagoManual')
     Spree::PaymentMethod.where(type: 'PaymentMethod::MercadoPagoCustom').update_all(type: 'Spree::PaymentMethod::MercadoPagoCustom')
+
+    Spree::Payment.where(source_type: 'MercadoPagoManualSource').update_all(source_type: 'Spree::MercadoPagoManualSource')
   end
 end
