24a9d1a46663887c5fab3cf7a75d4769592eb725
Matias Garcia <mgarcia@devartis.com>
2015-05-14 15:17:15 -0300

    add attributes

--- db/migrate/20150514150038_create_spree_mercado_pago_custom_source.rb
+++ db/migrate/20150514150038_create_spree_mercado_pago_custom_source.rb
@@ -1,6 +1,9 @@
 class CreateSpreeMercadoPagoCustomSource < ActiveRecord::Migration
   def change
     create_table :spree_mercado_pago_custom_sources do |t|
+      t.string :card_token
+      t.string :integration_payment_method_id
+      t.integer :installments
     end
   end
 end
