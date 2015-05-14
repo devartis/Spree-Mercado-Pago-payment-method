771014a272934739ebab9e6ad7113e5699f65dbf
Matias Garcia <mgarcia@devartis.com>
2015-05-14 14:37:30 -0300

    customer

---
+++ app/models/spree/mercado_pago/custom_client/customer.rb
@@ -0,0 +1,21 @@
+class Spree::MercadoPago::CustomClient::Customer < Spree::MercadoPago::CustomClient::Resource
+  def build_create_params(customer_email)
+    {
+        email: customer_email
+    }
+  end
+
+  def associate_card(customer_id, token)
+    client.post customer_cards_endpoint(customer_id), {token: token}
+  end
+
+  def get_cards(customer_id)
+    client.get customer_cards_endpoint(customer_id)
+  end
+
+  private
+
+  def customer_cards_endpoint(customer_id)
+    self.endpoint + "/#{customer_id}/cards"
+  end
+end
