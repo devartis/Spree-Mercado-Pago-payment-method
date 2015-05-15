e41a75d41728ffe60bfca541c2bf64912e4006a6
Matias Garcia <mgarcia@devartis.com>
2015-05-15 12:46:34 -0300

    fix

--- db/migrate/20150514181629_add_mercado_pago_code_to_spree_decidir_credit_card_types.rb
+++ db/migrate/20150514181629_add_mercado_pago_code_to_spree_decidir_credit_card_types.rb
@@ -1,17 +1,19 @@
 class AddMercadoPagoCodeToSpreeDecidirCreditCardTypes < ActiveRecord::Migration
   def change
     add_column :spree_decidir_credit_card_types, :mercado_pago_code, :string
+    
+    klass = SpreeDecidir::CreditCardType
 
-    SpreeDecidir::CreditCardType.create! name: 'Nativa Mastercard', decidir_code: '', mercado_pago_code: 'nativa', code: 'NT'
-    SpreeDecidir::CreditCardTypes.find(1).update!(mercado_pago_code: 'visa')
-    SpreeDecidir::CreditCardTypes.find(2).update!(mercado_pago_code: 'amex')
-    SpreeDecidir::CreditCardTypes.find(4).update!(mercado_pago_code: 'mastercard')
-    SpreeDecidir::CreditCardTypes.find(21).update!(mercado_pago_code: 'argencard')
-    SpreeDecidir::CreditCardTypes.find(9).update!(mercado_pago_code: 'cabal')
-    SpreeDecidir::CreditCardTypes.find(3).update!(mercado_pago_code: 'diners')
-    SpreeDecidir::CreditCardTypes.find(6).update!(mercado_pago_code: 'naranja')
-    SpreeDecidir::CreditCardTypes.find(5).update!(mercado_pago_code: 'tarshop')
-    SpreeDecidir::CreditCardTypes.find(7).update!(mercado_pago_code: 'pagofacil')
-    SpreeDecidir::CreditCardTypes.find(8).update!(mercado_pago_code: 'rapipago')
+    klass.create! name: 'Nativa Mastercard', decidir_code: 15, mercado_pago_code: 'nativa', code: 'NT'
+    klass.find(1).update!(mercado_pago_code: 'visa')
+    klass.find(2).update!(mercado_pago_code: 'amex')
+    klass.find(4).update!(mercado_pago_code: 'mastercard')
+    klass.find(21).update!(mercado_pago_code: 'argencard')
+    klass.find(9).update!(mercado_pago_code: 'cabal')
+    klass.find(3).update!(mercado_pago_code: 'diners')
+    klass.find(6).update!(mercado_pago_code: 'naranja')
+    klass.find(5).update!(mercado_pago_code: 'tarshop')
+    klass.find(7).update!(mercado_pago_code: 'pagofacil')
+    klass.find(8).update!(mercado_pago_code: 'rapipago')
   end
 end
