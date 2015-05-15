75186d35c18ee06d4a0b02482ee42f33ce1a292d
Matias Garcia <mgarcia@devartis.com>
2015-05-15 15:56:47 -0300

    use installment plan presenter

--- app/misc/presenters/spree/mercado_pago/financial_corporation_presenter.rb
+++ app/misc/presenters/spree/mercado_pago/financial_corporation_presenter.rb
@@ -6,6 +6,8 @@ class Spree::MercadoPago::FinancialCorporationPresenter
     @id = nil
     @code = nil
     @name = financial_corporation[:name]
-    @installment_plans = financial_corporation[:installment_plans]
+    @installment_plans =financial_corporation[:installment_plans].collect do |installment_plan|
+      Spree::MercadoPago::InstallmentPlanPresenter.new installment_plan
+    end
   end
 end
--- app/misc/presenters/spree/mercado_pago/installment_plan_presenter.rb
+++ app/misc/presenters/spree/mercado_pago/installment_plan_presenter.rb
@@ -1,3 +1,5 @@
 class Spree::MercadoPago::InstallmentPlanPresenter
+  def initialize(installment_plan)
 
+  end
 end
