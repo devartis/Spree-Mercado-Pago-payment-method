fbc94fd64a18c64b5693e2cf0c0327b231525592
Matias Garcia <mgarcia@devartis.com>
2015-05-15 16:16:36 -0300

    InstallmentPlanPresenter implementation

--- app/misc/presenters/spree/mercado_pago/installment_plan_presenter.rb
+++ app/misc/presenters/spree/mercado_pago/installment_plan_presenter.rb
@@ -1,5 +1,11 @@
 class Spree::MercadoPago::InstallmentPlanPresenter
-  def initialize(installment_plan)
+  attr_accessor :installments, :discount_percentage, :interest_percentage, :order
+  attr_accessor :id, :financial_corporation_id, :credit_card_type_id, :description #Added in order to render the object
 
+  def initialize(installment_plan)
+    @installments = installment_plan[:installments].to_i
+    @discount_percentage = installment_plan[:disccount_rate].to_f
+    @interest_percentage = installment_plan[:installment_rate].to_f
+    @order = SpreeDecidir::InstallmentPlanOrderCalculator.new(interest_percentage, discount_percentage, installments)
   end
 end
