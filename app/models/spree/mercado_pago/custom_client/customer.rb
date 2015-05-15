71d7a1043d9640c08b010017616a70f090a1192e
Matias Garcia <mgarcia@devartis.com>
2015-05-15 10:42:27 -0300

    find_or_create method

--- app/models/spree/mercado_pago/custom_client/customer.rb
+++ app/models/spree/mercado_pago/custom_client/customer.rb
@@ -3,6 +3,17 @@ class Spree::MercadoPago::CustomClient::Customer < Spree::MercadoPago::CustomCli
     {email: customer_email}
   end
 
+  def find_or_create(customer_email)
+    params = {email: customer_email}
+    response = create(params)[:response]
+    if response and response[:error] and response[:cause].first[:code].to_i == 101
+      saved_customer_response = do_get search_endpoint, params
+      saved_customer_response[:response][:id]
+    else
+      response[:id]
+    end
+  end
+
   def associate_card(customer_id, token)
     do_post customer_cards_endpoint(customer_id), {token: token}
   end
@@ -15,5 +26,9 @@ class Spree::MercadoPago::CustomClient::Customer < Spree::MercadoPago::CustomCli
 
   def customer_cards_endpoint(customer_id)
     "#{self.endpoint(customer_id)}/cards"
+  end
+
+  def search_endpoint
+    "#{self.endpoint}/search"
   end
 end
