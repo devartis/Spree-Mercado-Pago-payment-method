7ee6c31b8693aae3c8f88dc3b4c24cb6b1f96239
Matias Garcia <mgarcia@devartis.com>
2015-05-15 16:16:28 -0300

    get best plan

--- app/misc/presenters/spree/mercado_pago/financial_corporation_presenter.rb
+++ app/misc/presenters/spree/mercado_pago/financial_corporation_presenter.rb
@@ -1,13 +1,14 @@
 class Spree::MercadoPago::FinancialCorporationPresenter
   extend Forwardable
-  attr_accessor :id, :name, :code, :installment_plans
+  attr_accessor :id, :name, :code, :installment_plans, :best_plan
 
   def initialize(financial_corporation)
     @id = nil
     @code = nil
     @name = financial_corporation[:name]
-    @installment_plans =financial_corporation[:installment_plans].collect do |installment_plan|
+    @installment_plans = financial_corporation[:installment_plans].collect do |installment_plan|
       Spree::MercadoPago::InstallmentPlanPresenter.new installment_plan
     end
+    @best_plan = @installment_plans.min_by { |ip| ip.order }
   end
 end
