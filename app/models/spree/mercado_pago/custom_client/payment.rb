08de9125cdcaf4e2f8445ffacdc94ca9ec9eccd3
Matias Garcia <mgarcia@devartis.com>
2015-05-15 11:52:47 -0300

    refactor

--- app/models/spree/mercado_pago/custom_client/payment.rb
+++ app/models/spree/mercado_pago/custom_client/payment.rb
@@ -1,15 +1,15 @@
 class Spree::MercadoPago::CustomClient::Payment < Spree::MercadoPago::CustomClient::Resource
   def build_create_params(amount, card_token, description, installments, options={})
-    if options.keys.size == 2 and options[:payment_method_id] and options[:payer_email]
-      diff_params = {
-          payment_method_id: options[:payment_method_id],
-          payer: {email: options[:payer_email]}
-      }
-    elsif options.keys.size == 1 and options[:payer_id]
-      diff_params = {
-          payer: {id: options[:payer_id]}
-      }
-    end
+    diff_params = if options.keys.size == 2 and options[:payment_method_id] and options[:payer_email]
+                    {
+                        payment_method_id: options[:payment_method_id],
+                        payer: {email: options[:payer_email]}
+                    }
+                  elsif options.keys.size == 1 and options[:payer_id]
+                    {
+                        payer: {id: options[:payer_id]}
+                    }
+                  end
     common_params = {
         transaction_amount: amount,
         token: card_token,
