48c17f4f0311653366f5a9534211b5dee5f187e9
Matias Garcia <mgarcia@devartis.com>
2015-05-18 11:48:19 -0300

    add description fix calculate

--- app/misc/presenters/spree/mercado_pago/installment_plan_presenter.rb
+++ app/misc/presenters/spree/mercado_pago/installment_plan_presenter.rb
@@ -1,11 +1,12 @@
 class Spree::MercadoPago::InstallmentPlanPresenter
-  attr_accessor :installments, :discount_percentage, :interest_percentage, :order
-  attr_accessor :id, :financial_corporation_id, :credit_card_type_id, :description #Added in order to render the object
+  attr_accessor :installments, :discount_percentage, :interest_percentage, :order, :description
+  attr_accessor :id, :financial_corporation_id, :credit_card_type_id #Added in order to render the object
 
   def initialize(installment_plan)
     @installments = installment_plan[:installments].to_i
     @discount_percentage = installment_plan[:disccount_rate].to_f
     @interest_percentage = installment_plan[:installment_rate].to_f
-    @order = SpreeDecidir::InstallmentPlanOrderCalculator.new(interest_percentage, discount_percentage, installments)
+    @description = installment_plan[:recommended_message]
+    @order = SpreeDecidir::InstallmentPlanOrderCalculator.new(interest_percentage, discount_percentage, installments).calculate
   end
 end
