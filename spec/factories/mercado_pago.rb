bc6ce673aa37308d24a915c8b63638468e794f5b
Matias Garcia <mgarcia@devartis.com>
2015-05-18 13:02:39 -0300

    factories

--- spec/factories/mercado_pago.rb
+++ spec/factories/mercado_pago.rb
@@ -8,4 +8,10 @@ FactoryGirl.define do
     preferred_client_id 'test'
     preferred_client_secret 'test'
   end
+
+  factory :mercado_pago_custom_payment_method, class: Spree::PaymentMethod::MercadoPagoCustom do
+    sandbox true
+    preferred_public_key_sandbox 'TEST-5d26c80f-90fc-4921-8a5d-427cc2531d6a'
+    preferred_access_token_sandbox 'TEST-3399440369171872-051314-774e7f2878bc1154ea77c02b3ff9af00__LD_LA__-74349919'
+  end
 end
