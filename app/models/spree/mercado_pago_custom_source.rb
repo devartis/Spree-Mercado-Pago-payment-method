46a729890320fec9a310d3719c89cf3a7d2f12ee
Matias Garcia <mgarcia@devartis.com>
2015-05-18 15:35:14 -0300

    annotate

--- app/models/spree/mercado_pago_custom_source.rb
+++ app/models/spree/mercado_pago_custom_source.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: spree_mercado_pago_custom_sources
+#
+#  id                               :integer          not null, primary key
+#  card_token                       :string(255)
+#  integration_payment_method_id    :string(255)
+#  installments                     :integer
+
+
 module Spree
   class MercadoPagoCustomSource < ActiveRecord::Base
 
