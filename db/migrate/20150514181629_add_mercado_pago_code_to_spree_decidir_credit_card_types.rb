0d15f17f25b708473043dd4281e6bcf90044b5ff
Matias Garcia <mgarcia@devartis.com>
2015-05-18 11:27:05 -0300

    update migration

--- db/migrate/20150514181629_add_mercado_pago_code_to_spree_decidir_credit_card_types.rb
+++ db/migrate/20150514181629_add_mercado_pago_code_to_spree_decidir_credit_card_types.rb
@@ -7,7 +7,7 @@ class AddMercadoPagoCodeToSpreeDecidirCreditCardTypes < ActiveRecord::Migration
     klass.create! name: 'Nativa Mastercard', decidir_code: 15, mercado_pago_code: 'nativa', code: 'NT'
     klass.find(1).update!(mercado_pago_code: 'visa')
     klass.find(2).update!(mercado_pago_code: 'amex')
-    klass.find(4).update!(mercado_pago_code: 'mastercard')
+    klass.find(4).update!(mercado_pago_code: 'master')
     klass.find(21).update!(mercado_pago_code: 'argencard')
     klass.find(9).update!(mercado_pago_code: 'cabal')
     klass.find(3).update!(mercado_pago_code: 'diners')
