749f0fda412122c59c6657a0dacf8f5ae40ca248
Matias Garcia <mgarcia@devartis.com>
2015-07-23 11:13:19 -0300

    add document to mercado pago custom source

--- app/models/spree/mercado_pago_custom_source.rb
+++ app/models/spree/mercado_pago_custom_source.rb
@@ -6,10 +6,13 @@
 #  card_token                       :string(255)
 #  integration_payment_method_id    :string(255)
 #  installments                     :integer
+#  document_type                    :integer          default(0)
+#  document_number                  :integer
 
 
 module Spree
   class MercadoPagoCustomSource < ActiveRecord::Base
+    enum document_type: [ :dni, :ci, :le, :lc, ]
     belongs_to :payment_method
     belongs_to :user
     has_many :payments, as: :source, class_name: '::Spree::Payment'
