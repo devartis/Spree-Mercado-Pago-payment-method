40f9a7bacf58ccce5fb1eb6dd7c4c0ed6903397d
matiasgarcia <mati92.ss@gmail.com>
2016-06-16 13:34:02 -0300

    Optionally send additional_info to MercadoPago when creating a payment. Save raw_response and current payment status.

--- app/models/spree/mercado_pago/custom_client/payment.rb
+++ app/models/spree/mercado_pago/custom_client/payment.rb
@@ -3,15 +3,15 @@ class Spree::MercadoPago::CustomClient::Payment < Spree::MercadoPago::CustomClie
     do_get '/v1/payments/search', params
   end
 
-  def build_create_params(amount, card_token, description, installments, options={})
-    diff_params = if options.keys.size == 2 and options[:payment_method_id] and options[:payer_email]
+  def build_create_params(amount, card_token, description, installments, additional_info, payer_options={})
+    diff_params = if payer_options.keys.size == 2 and payer_options[:payment_method_id] and payer_options[:payer_email]
                     {
-                        payment_method_id: options[:payment_method_id],
-                        payer: {email: options[:payer_email]}
+                        payment_method_id: payer_options[:payment_method_id],
+                        payer: {email: payer_options[:payer_email]}
                     }
-                  elsif options.keys.size == 1 and options[:payer_id]
+                  elsif payer_options.keys.size == 1 and payer_options[:payer_id]
                     {
-                        payer: {id: options[:payer_id]}
+                        payer: {id: payer_options[:payer_id]}
                     }
                   end
     common_params = {
@@ -20,6 +20,6 @@ class Spree::MercadoPago::CustomClient::Payment < Spree::MercadoPago::CustomClie
         description: description,
         installments: installments,
     }
-    common_params.merge(diff_params)
+    common_params.merge(diff_params).merge(additional_info)
   end
 end
