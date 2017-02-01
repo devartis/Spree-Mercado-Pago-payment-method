9c84eb49c73b47912d4c6e1112c9a39aa57205c6
guillermo coro <gcoro@devartis.com>
2017-02-01 13:57:29 -0300

    Consume cft and tea form mercadopago api

--- app/presenters/spree/mercado_pago/financial_corporation_presenter.rb
+++ app/presenters/spree/mercado_pago/financial_corporation_presenter.rb
@@ -10,9 +10,23 @@ module Spree
         @image = financial_corporation[:secure_thumbnail]
         @name = financial_corporation[:name]
         @installment_plans = financial_corporation[:installment_plans].collect do |ip|
-          SpreeDecidir::InstallmentPlan.new discount_percentage: ip[:disccount_rate].to_f, interest_percentage: ip[:installment_rate].to_f, installments: ip[:installments]
+
+          cft = parse_label(ip[:labels][0], 'cft', 0)
+          tea = parse_label(ip[:labels][0], 'tea', 1)
+
+          SpreeDecidir::InstallmentPlan.new(discount_percentage: ip[:disccount_rate].to_f,
+                                            interest_percentage: ip[:installment_rate].to_f,
+                                            installments: ip[:installments], cft: cft, tea: tea)
         end
         @best_plan = @installment_plans.min_by { |ip| ip.order }
+      end
+
+      def parse_label(label, attr, position)
+        label_value = 0  # default value
+        label_with_attr = label.split('|')[position] if label.present?
+        label_number = label_with_attr[/.*\_(.*?)%/,1] if label_with_attr.present? && label_with_attr.include?(attr.upcase)
+        label_value = label_number.gsub(',','.').to_f if label_number.present?
+        label_value
       end
     end
   end
